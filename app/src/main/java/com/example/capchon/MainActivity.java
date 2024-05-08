9074f597cb7813f61af43d394bb397280d6bc2d1
yunho <yunhoo0511@naver.com>
2024-05-08 16:16:01 +0900

    메인화면 구성 및 로그인 페이지만 들기

--- app/src/main/java/com/example/capchon/MainActivity.java
+++ app/src/main/java/com/example/capchon/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.capchon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //준형테스트
-        //민혁테스트
+        Button btn_q = findViewById(R.id.btn_q);
+        btn_q.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), QuestActivity.class);
+            }
+        });
+        Button btn_w = findViewById(R.id.btn_w);
+        btn_w.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WalkActivity.class);
+            }
+        });
+        Button btn_m = findViewById(R.id.btn_m);
+        btn_m.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+            }
+        });
+        Button btn_c = findViewById(R.id.btn_c);
+        btn_c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CommunicationActivity.class);
+            }
+        });
+
     }
+
+
 }
