2c456100b62a7f80c04dcb70e2e0f03e40da60f2
yunho <yunhoo0511@naver.com>
2024-05-13 14:59:07 +0900

    블루투스 연동 및 걸음수 체크

--- app/src/main/java/com/example/capchon/WalkActivity.java
+++ app/src/main/java/com/example/capchon/WalkActivity.java
@@ -9,15 +9,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Set;
 import java.util.UUID;
-//
 
 public class WalkActivity extends AppCompatActivity {
     private BluetoothAdapter bluetoothAdapter;
