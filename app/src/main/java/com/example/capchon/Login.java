2a1acdb4eb10379c97d7ec372a21d92a4187a99e
yunho <yunhoo0511@naver.com>
2024-05-20 16:02:10 +0900

    로그인 데이터베이스 코딩작성중

--- app/src/main/java/com/example/capchon/Login.java
+++ app/src/main/java/com/example/capchon/Login.java
@@ -6,16 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Login extends AppCompatActivity {
     TextView sign;
     Button loginbutton;
+    private FirebaseAuth mFirebaseAuth; //파이어베이스 인증
+    private DatabaseReference mDatabaseRef ; //실시간데이터베이스
+    private EditText editText, ediPassword;
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
 
         //회원가입 버튼
         sign = findViewById(R.id.signin);
