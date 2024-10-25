c98553376d342d4e82032d05e943238f5c82e143
yunho <yunhoo0511@naver.com>
2024-10-25 14:48:31 +0900

    현재위치랑 목적지 정함2

--- app/src/main/java/com/example/capchon/GeocodingResponse.java
+++ app/src/main/java/com/example/capchon/GeocodingResponse.java
@@ -3,10 +3,12 @@ package com.example.capchon;
 import java.util.List;
 
 public class GeocodingResponse {
-    public List<Address> addresses; // 주소 정보 리스트
+    public List<GeocodingResult> addresses;
 
-    public static class Address {
-        public String x; // 경도 (Longitude)
-        public String y; // 위도 (Latitude)
+    public static class GeocodingResult {
+        public String roadAddress;
+        public String jibunAddress;
+        public Double x; // 경도
+        public Double y; // 위도
     }
 }
