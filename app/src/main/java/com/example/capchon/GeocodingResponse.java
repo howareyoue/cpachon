e672d7f558bb6c8aaf9506ef5a9a053f5c68ccea
yunho <yunhoo0511@naver.com>
2024-10-25 13:20:10 +0900

    api키 수정한 지도

---
+++ app/src/main/java/com/example/capchon/GeocodingResponse.java
@@ -0,0 +1,14 @@
+package com.example.capchon;
+
+import java.util.List;
+
+public class GeocodingResponse {
+
+    public List<Address> addresses; // 주소 정보 리스트
+
+    public static class Address {
+        public String x; // 경도 (Longitude)
+        public String y; // 위도 (Latitude)
+    }
+}
+
